9312e9102e868da6bcfe7baa70dd341496bf7911
Harvey Kelly <harveyjk86@gmail.com>
2019-07-16 13:35:31 +0000

    Added all CRUD methods to controller

--- src/main/java/appointments/lambda/controller/AppointmentController.java
+++ src/main/java/appointments/lambda/controller/AppointmentController.java
@@ -8,10 +8,13 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -72,4 +75,77 @@ public final class AppointmentController {
     return appointment;
   }
 
+  @RequestMapping(
+    method = RequestMethod.GET,
+    path = "/appointments/read/{id}",
+    produces = "application/json"
+  )
+  public Appointment appointmentsRead(
+    @PathVariable
+    final UUID id
+  ) {
+    if (id == null) {
+      throw new IllegalArgumentException("Illegal argument; id cannot be "
+        + "null.");
+    }
+    String message = String.format("appointmentsRead[id==%s]", id);
+    LOGGER.info(message);
+    final Optional<Appointment> result = this.repository.findById(id);
+    final Appointment appointment = result.get();
+    message = String.format("appointmentsRead returns %s", appointment);
+    LOGGER.info(message);
+    return appointment;
+  }
+
+  @RequestMapping(
+    method = RequestMethod.PUT,
+    path = "/appointments/update/{id}",
+    produces = "application/json"
+  )
+  public Appointment appointmentsUpdate(
+    @PathVariable
+    final UUID id,
+    @RequestBody
+    final String json
+  ) throws IOException {
+    if (id == null || json == null || "".equals(json.trim())) {
+      final String message = String.format("Illegal argument; id cannot be "
+        + "null and json cannot be null, empty or only whitespace characters. "
+        + "[id==%s, json==%s]", id, json);
+      throw new IllegalArgumentException(message);
+    }
+    String message = String.format("appointmentsUpdate[id==%s, json==\"%s\"]",
+      id, json);
+    LOGGER.info(message);
+    Appointment appointment = OBJECT_MAPPER.readValue(json, Appointment.class);
+    appointment.setId(id);
+    appointment = this.repository.save(appointment);
+    message = String.format("appointmentsUpdate returns %s", appointment);
+    LOGGER.info(message);
+    return appointment;
+  }
+
+  @RequestMapping(
+    method = RequestMethod.DELETE,
+    path = "/appointments/delete/{id}",
+    produces = "application/json"
+  )
+  public List<Appointment> appointmentsDelete(
+    @PathVariable
+    final UUID id
+  ) throws IOException {
+    if (id == null) {
+      throw new IllegalArgumentException("Illegal argumentl id cannot be "
+        + "null.");
+    }
+    String message = String.format("appointmentsDelete[id==%s]", id);
+    LOGGER.info(message);
+    this.repository.deleteById(id);
+    final List<Appointment> appointments =
+      this.repository.findAllByOrderByDateAsc();
+    message = String.format("appointmentsDelete returns %s", appointments);
+    LOGGER.info(message);
+    return appointments;
+  }
+ 
 }
--- src/main/java/appointments/lambda/repository/AppointmentRepository.java
+++ src/main/java/appointments/lambda/repository/AppointmentRepository.java
@@ -3,11 +3,12 @@ package appointments.lambda.repository;
 import appointments.lambda.entity.Appointment;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.data.repository.CrudRepository;
 
 public interface AppointmentRepository
-  extends CrudRepository<Appointment, Long> {
+  extends CrudRepository<Appointment, UUID> {
 
   List<Appointment> findAllByOrderByDateAsc();
 
